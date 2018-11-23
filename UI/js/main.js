aebb5d561da879b13a818078f5b36d8ca1d8f6f7
lawrenceChege <lawrencechege.s@gmail.com>
2018-11-23 10:05:41 +0300

    [ft #162099567] add responsive tabs and profile card

--- UI/js/main.js
+++ UI/js/main.js
@@ -20,7 +20,7 @@ function openPage(pageName, elmnt, color) {
   }
 
   // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
+  document.getElementById(pageName).style.display = "flex";
 
   // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
