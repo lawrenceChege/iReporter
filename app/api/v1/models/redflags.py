29951d3226abf8347eedb686e26fd87e3a4fc2e6
lawrenceChege <lawrencechege.s@gmail.com>
2018-11-27 17:14:03 +0300

    [fix #162188475] refactor get all redflags

---
+++ app/api/v1/models/redflags.py
@@ -0,0 +1,6 @@
+""""  """
+REDFLAGS = []
+class Helper():
+    def get_all(self):
+        return {"status":200, "data": REDFLAGS, "message":"All redflags found successfully"}
+
--- app/api/v1/views/redflags.py
+++ app/api/v1/views/redflags.py
@@ -1,17 +1,13 @@
 """ These module deals with redflag methods and routes"""
 import datetime
-from flask-restplus import Resource, reqparse
+from flask-restplus import Resource
 from flask import request
-
-REDFLAGS = []
+from app.api.v1.models.redflags import Helper
 
 class RedFlags(Resource):
     """
         This class has methods for posting redflags and getting all redflags posted
     """
-    def get(self):
-        pass
-
     def post(self):
         self.parser = reqparse.RequestParser()
         self.parser.add_argument("title", type=str, help=" the title of the record")
@@ -34,7 +30,8 @@ class RedFlags(Resource):
         return {"status": 201, "data": REDFLAGS}
 
     def get(self):
-        return {"status": 200, "data": REDFLAGS}
+        redflags = Helper().get_all()
+        return redflags 
         
 
         
