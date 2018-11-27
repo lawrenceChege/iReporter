94c8ecad82f606ac54a119399f0d7b6874bec632
lawrenceChege <lawrencechege.s@gmail.com>
2018-11-27 18:05:44 +0300

    [fix #162188475] update create redflag

--- app/api/v1/views/redflags.py
+++ app/api/v1/views/redflags.py
@@ -1,35 +1,17 @@
 """ These module deals with redflag methods and routes"""
 import datetime
-from flask-restplus import Resource, reqparse
+from flask-restplus import Resource
 from flask import request
-
-REDFLAGS = []
+from app.api.v1.models.redflags import Helper
 
 class RedFlags(Resource):
     """
         This class has methods for posting redflags and getting all redflags posted
     """
-    def get(self):
-        pass
-
     def post(self):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument("title", type=str, help=" the title of the record")
-        self.parser.add_argument("location", type=str, help=" the location of the record")
-        self.parser.add_argument("comment", type=str, help="Body is required")
-        args = self.parser.parse_args()   
+        post_redflag = Helper().new_redflag()
+        return post_redflag
 
-
-        REDFLAG = {
-                "id": len(REDFLAGS)+1,
-                "createdOn" : datetime.now,  
-                "createdBy" : "carolmobic",
-                "type" : "RedFlag",
-                "title": args["title"],
-                "location" : args["location"],
-                "status": "pending",
-                "comment" : args["comment"]
-            }
-        REDFLAGS.append(REDFLAG)
-        return REDFLAGS,201
-        
+    def get(self):
+        redflags = Helper().get_all()
+        return redflags 
