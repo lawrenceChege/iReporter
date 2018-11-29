520794cb6892c69575927019ac853c5881eafb42
lawrenceChege <lawrencechege.s@gmail.com>
2018-11-29 09:45:20 +0300

    [finishes #162260328] create methods for user login

--- app/api/v1/models/users.py
+++ app/api/v1/models/users.py
@@ -3,7 +3,7 @@ from flask_restful import reqparse
 USERS = []
 
 class Helper():
-    def post_user():
+    def signup_user(self):
         user ={
             "id" : len(USER)+1, 
            "firstname" : request.json["firstname"],
@@ -11,8 +11,19 @@ class Helper():
            "email" : request.json["email"],
            "phoneNumber" : request.json["phoneNumber"],
            "username" : request.json["username"], 
+           "password": request.json["password"],
            "registered" : datetime.datetime.now,
            "isAdmin" : False,
         }
         USERS.append(user)
         return {"status":201, "data": USERS, "message":"User successfully created"}
+
+    def login_user(self, user_id):
+        for user in USERS:
+            if user["id"]= user_id:
+                if user["password"]== request.json["password"]:
+                    return {"status": 200, "message":"successful"}
+                else:
+                    return {"status": 401, "message": "wrong credntials!"}
+            else:
+                return {"status":404, "message": "user not found"}
--- app/api/v1/views/users.py
+++ app/api/v1/views/users.py
@@ -3,8 +3,16 @@ from flask import request
 
 class Users(Resource):
     """
-        This class defines methods for getting all users
+        This class defines methods for getting all users and signing up
     """
     def post(self):
-        user = Helper().post_user()
+        user_signup = Helper().signup_user()
         return user
+
+class User(Resource):
+    """
+        This class defines mthods for login in
+    """
+    def post(self, user_id):
+        user_login = Helper().login_user(user_id)
+        return user_login
