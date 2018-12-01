91a02cd41b48e70a824c05742a31a274ec93e88e
lawrenceChege <lawrencechege.s@gmail.com>
2018-12-01 09:37:09 +0300

    add methods for test and coverage commands

--- run.py
+++ run.py
@@ -1,6 +1,39 @@
+"""
+This module runs the app and tests 
+"""
+import os
+import click
+import subprocess
+
 from app import create_app
 
-app = create_app()
+app = create_app(os.getenv('FLASK_CONFIG')or default)
+
+@app.cli.command()
+@click.argument('path', default=os.path.join('app'. 'tests'))
+def test(path):
+    """
+        This fuction runs tests with pytest. 
+        It takes the test path as the parameter and
+        returns the subprocees call for tests as the result
+    """
+
+    cmd = 'py.test -v {0}'.format(path)
+    return subprocess.call(cmd, shell = True)
+
+
+@app.cli.command()
+@click.argument('path', default='app')
+def coverage(path):
+    """
+        This fuction generates a coverage report for the tests.
+        It takes the test path as the parameter and
+        returns the subprocess call for coverage as the result
+    """
+
+    cmd = 'py.test -v --cov-report term-missing --cov {0}'.format(path)
+    return subprocess.call(cmd, shell = True)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
