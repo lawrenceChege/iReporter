a716768ce483e96fdb54f4f4b580d0420867e165
lawrenceChege <lawrencechege.s@gmail.com>
2018-12-01 17:52:15 +0300

    update run.py

--- run.py
+++ run.py
@@ -7,33 +7,33 @@ import subprocess
 
 from app import create_app
 
-app = create_app(os.getenv('FLASK_CONFIG')or default)
+APP = create_app(os.getenv('FLASK_CONFIG'))
 
-@app.cli.command()
-@click.argument('path', default=os.path.join('app'. 'tests'))
-def test(path):
-    """
-        This fuction runs tests with pytest. 
-        It takes the test path as the parameter and
-        returns the subprocees call for tests as the result
-    """
+# @app.cli.command()
+# @click.argument('path', default=os.path.join('app'. 'tests'))
+# def test(path):
+#     """
+#         This fuction runs tests with pytest. 
+#         It takes the test path as the parameter and
+#         returns the subprocees call for tests as the result
+#     """
 
-    cmd = 'py.test -v {0}'.format(path)
-    return subprocess.call(cmd, shell = True)
+#     cmd = 'py.test -v {0}'.format(path)
+#     return subprocess.call(cmd, shell = True)
 
 
-@app.cli.command()
-@click.argument('path', default='app')
-def coverage(path):
-    """
-        This fuction generates a coverage report for the tests.
-        It takes the test path as the parameter and
-        returns the subprocess call for coverage as the result
-    """
+# @app.cli.command()
+# @click.argument('path', default='app')
+# def coverage(path):
+#     """
+#         This fuction generates a coverage report for the tests.
+#         It takes the test path as the parameter and
+#         returns the subprocess call for coverage as the result
+#     """
 
-    cmd = 'py.test -v --cov-report term-missing --cov {0}'.format(path)
-    return subprocess.call(cmd, shell = True)
+#     cmd = 'py.test -v --cov-report term-missing --cov {0}'.format(path)
+#     return subprocess.call(cmd, shell = True)
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    APP.run(debug = True)
