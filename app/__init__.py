7a9627b14ded3ee2b9f03d566d596c7ef87cbaa7
lawrenceChege <lawrencechege.s@gmail.com>
2018-12-01 09:56:39 +0300

    update createapp method to take config settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,20 @@
+"""
+    This module cnfigures how the app will run
+"""
 from flask import Flask, Blueprint
 from flask_restplus import Api
+from instance.config import config
 
 from app.api.v1 import version_one as v1
 
-def create_app():
-    APP= Flask(__name__)
+def create_app(config_name):
+    """
+        This function creates a flask app and
+        registers api version blueprints 
+    """
+    APP= Flask(__name__, instance_relative_config=True)
+    APP.config.from_object(config[config_name])
+    APP.config.from_pyfile('config.py')
     APP.register_blueprint(v1)
     return APP
 
