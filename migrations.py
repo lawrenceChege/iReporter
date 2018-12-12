b9c544b449d7ce391e1ec5d1facd702221db1ecb
lawrenceChege <lawrencechege.s@gmail.com>
2018-12-12 09:10:28 +0300

    configure tests

--- migrations.py
+++ migrations.py
@@ -2,34 +2,40 @@
 import os
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from flask import current_app
 
-HEROKU_DB_URL = 'postgres://gqpylxymapzcup:ebd641e7c5bd2116a79f179a869557f684818b0df68a0379a528330987886192@ec2-184-72-239-186.compute-1.amazonaws.com:5432/d2hukbh74b0mkk'
-DATABASE_URL = 'postgresql://localhost/ireporter?user=postgres&password=12345678'
-TEST_DATABASE_URL = 'postgresql://localhost/ireporter_test?user=postgres&password=12345678'
-CONNECTION_CREDS = {
-    "host": os.getenv('DB_HOST'),
-    "database": os.getenv('DB_NAME'),
-    "user": os.getenv('DB_USER'),
-    "password": os.getenv('DB_PASSWORD')
-}
-TEST_CONNECTION_CREDS = {
-    "host": os.getenv('TEST_DB_HOST'),
-    "database": os.getenv('TEST_DB_NAME'),
-    "user": os.getenv('TEST_DB_USER'),
-    "password": os.getenv('TEST_DB_PASSWORD')
-}
+# DATABASE_URL = 'postgresql://localhost/ireporter?user=postgres&password=12345678'
+# TEST_DATABASE_URL = 'postgresql://localhost/ireporter_test?user=postgres&password=12345678'
+# CONNECTION_CREDS = {
+#     "host": os.getenv('DB_HOST'),    
+#     "database": os.getenv('DB_NAME'),
+#     "user": os.getenv('DB_USER'),
+#     "password": os.getenv('DB_PASSWORD')
+# }
+# TEST_CONNECTION_CREDS = {
+#     "host": os.getenv('TEST_DB_HOST'),    
+#     "database": os.getenv('TEST_DB_NAME'),
+#     "user": os.getenv('TEST_DB_USER'),
+#     "password": os.getenv('TEST_DB_PASSWORD')
+# }
 
 class DbModel():
     """
         consists of methods to connect and query from db
     """
     def __init__(self, db):
-        self.db_url = DATABASE_URL
-        self.db_test_url = TEST_DATABASE_URL
-        self.db_con_creds = CONNECTION_CREDS
-        self.db_heroku = HEROKU_DB_URL
-        self.db_test_con_creds = TEST_CONNECTION_CREDS
-        self.conn = self.choose_db(db)
+        self.db_host = current_app.config['DB_HOST']
+        self.db_username = current_app.config['DB_USERNAME']
+        self.db_password = current_app.config['DB_PASSWORD']
+        self.db_name = current_app.config['DB_NAME']
+        self.db_url = current_app.config['DATABASE_URL']
+        self.conn_cred = {
+            "host": self.db_host,
+            "database": self.db_name,
+            "user": self.db_username,
+            "password": self.db_password
+        }
+        self.conn = self.connection(self.conn_cred)
         self.cur = self.conn.cursor(cursor_factory=RealDictCursor)
 
     def connection(self, *args, **url):
@@ -50,42 +56,14 @@ class DbModel():
                 print('connected to heroku db\n')
                 return conn
             except:
-                conn = psycopg2.connect(
-                    'postgresql://localhost/ireporter?user=postgres&password=12345678')
+                conn = psycopg2.connect(self.conn_cred)
                 print('connected to db\n')
                 return conn
         except (Exception, psycopg2.DatabaseError) as error:
             print(error)
             print('error connecting to db\n')
 
-    def init_test_db(self):
-        """
-            connect to test db
-        """
-        try:
-            print("connecting to test db...\n")
-            try:
-                conn = connection(self.db_test_url)
-                print('connected to test db\n')
-                return conn
-            except:
-                conn = psycopg2.connect(
-                    'postgresql://localhost/ireporter_test?user=postgres&password=12345678')
-                print('connected to test db\n')
-                return conn
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-            print('error connecting to test db\n')
-
-    def choose_db(self,db):
-        """ choose database to connect to """
-        if db == "main":
-            conn = self.init_db()
-            return conn
-        elif db == "test":
-            conn = self.init_test_db()
-            return conn
-
+   
     def create_tables(self):
         """
             create tables in the database
