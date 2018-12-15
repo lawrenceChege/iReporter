061dc2d515a075dabde319a1c733be0449768f2f
lawrenceChege <lawrencechege.s@gmail.com>
2018-12-15 23:34:38 +0300

    [finishes #162683953] add function for user signup

---
+++ UI/js/fetch.js
@@ -0,0 +1,22 @@
+const registerUser = () => {
+    fetch('https://ireporti.herokuapp.com/api/v2/auth/signup/', {
+      method: 'POST',
+      body: JSON.stringify({
+        username: document.getElementById('username').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value
+      }),
+      headers: {
+        'Content-type': 'application/json;'
+      }
+      })
+      .then(response => response.json())
+      .then(registerData => {
+        if(registerData.message === "User created successfully"){
+          window.location.href = "./profile.html";
+          sessionStorage.setItem("token",registerData.token);
+        }else{
+            document.getElementById('error').innerHTML = registerData.message;
+        }
+      })
+  } 
